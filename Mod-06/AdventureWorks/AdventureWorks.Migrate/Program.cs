2bf217091bf48ad36b9b78aefb9915e7aebd02da
Jose Rene Fuentes <jrfuent@gmail.com>
2021-01-20 01:03:31 +0100

    Mod y MOC han sido agregados

---
+++ Mod-06/AdventureWorks/AdventureWorks.Migrate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventureWorks.Migrate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
